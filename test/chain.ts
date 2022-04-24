1497f8a06c5d53256198b9f1df127ebf16a94bcd
1304082170@qq.com <1304082170@qq.com>
2022-04-24 11:52:46 +0800

    Feat test

---
+++ test/chain.ts
@@ -0,0 +1,3 @@
+import { chain, VERSION, subtract, divide, multiply, addition } from '../dist'
+
+console.log(chain().addition(1).addition(2).getValue())
---
+++ test/index.js
@@ -0,0 +1,5 @@
+const { subtract } = require('../dist')
+
+console.log(0.1 - 0.3, '\n', '===>')
+
+console.log(subtract(0.1, 0.3))
